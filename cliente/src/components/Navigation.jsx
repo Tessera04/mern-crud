4df9d3689c1bbcf153974d153089c127d9702b80
Matias <matute_11@hotmail.com.ar>
2024-12-09 05:08:06 -0300

    Navigation Menu

--- cliente/src/components/Navigation.jsx
+++ cliente/src/components/Navigation.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
 export default function Navigation() {
   return (
     <Navbar collapseOnSelect expand="lg" variant='dark' bg='dark'>
-      <h1>Navigation</h1>
+        <Navbar.Brand as={NavLink} to="/">Task Manager</Navbar.Brand>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav'></Navbar.Toggle>
+        <Navbar.Collapse id='responsive-navbar-nav'>
+            <Nav className='me-auto'>
+                <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+                <Nav.Link as={NavLink} to="/projects">Projects</Nav.Link>
+                <NavDropdown title="Admin">
+                    <NavDropdown.Item as={NavLink} to="/admin/users">Usuarios</NavDropdown.Item>
+                </NavDropdown>
+            </Nav>
+            <Nav>
+                <Nav.Link as={NavLink} to="/login">Login</Nav.Link>
+                <Nav.Link as={NavLink} to="/register">Register</Nav.Link>
+                <Nav.Link as={NavLink} to="/account">Account</Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
     </Navbar>
   )
 }
